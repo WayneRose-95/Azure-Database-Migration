17973172b2d4ceee33b66ed273c1c19aba3c2bb1
WayneRose-95 <e.guishardrose@hotmail.co.uk>
2024-02-16 01:35:11 +0000

    Adding T-SQL script to Restore Database Script.sql file

---
+++ sql_scripts/Restore Database Script.sql
@@ -0,0 +1,4 @@
+USE [master]
+RESTORE DATABASE [AdventureWorks2022] FROM  DISK = N'C:\Program Files\Microsoft SQL Server\MSSQL16.MSSQLSERVER\MSSQL\Backup\AdventureWorks2022.bak' WITH  FILE = 1,  NOUNLOAD,  STATS = 5
+
+GO
